1cd2ab0ffcb07895a5785cd35ab8b3168fe57f21
xgr369 <example@google.com>
2026-08-17 10:18:34 -0400

    rename charlist to langm_charlist

--- ProgrammingLanguage/charlist.c
+++ ProgrammingLanguage/charlist.c
@@ -1,7 +1,7 @@
 #include "charlist.h"
 #include <string.h>
 
-int _charlist_resize(CharList *pcv, size_t addLen) {
+static int charlist_resize(LangM_CharList *pcv, size_t addLen) {
 	while (pcv->length + addLen > pcv->size)
 		pcv->size = pcv->size ? pcv->size * 2 : 1;
 	void* ptr = realloc(pcv->data, pcv->size);
@@ -11,7 +11,7 @@ int _charlist_resize(CharList *pcv, size_t addLen) {
 	return 0;
 }
 
-int charlist_init(CharList *pcv, size_t size) {
+int langM_charlist_init(LangM_CharList *pcv, size_t size) {
 	if (!pcv)
 		return 1;
 	pcv->data = malloc(size);
@@ -22,7 +22,7 @@ int charlist_init(CharList *pcv, size_t size) {
 	return 0;
 }
 
-int charlist_free(CharList *pcv) {
+int langM_charlist_free(LangM_CharList *pcv) {
 	if (!pcv || !pcv->data)
 		return 1;
 	free(pcv->data);
@@ -30,14 +30,14 @@ int charlist_free(CharList *pcv) {
 	return 0;
 }
 
-int charlist_get(CharList *pcv, size_t index, char *dst) {
+int langM_charlist_get(LangM_CharList *pcv, size_t index, char *dst) {
 	if (!pcv || !dst || index < 0 || index >= pcv->length)
 		return 1;
 	memcpy(dst, pcv->data + index, 1);
 	return 0;
 }
 
-int charlist_pop(CharList *pcv, char *dst) {
+int langM_charlist_pop(LangM_CharList *pcv, char *dst) {
 	if (!pcv || !dst || pcv->length == 0)
 		return 1;
 	memcpy(dst, pcv->data + pcv->length - 1, 1);
@@ -45,33 +45,33 @@ int charlist_pop(CharList *pcv, char *dst) {
 	return 0;
 }
 
-int charlist_push(CharList *pcv, char value) {
+int langM_charlist_push(LangM_CharList *pcv, char value) {
 	if (!pcv)
 		return 1;
 	if (pcv->length >= pcv->size)
-		if (_charlist_resize(pcv, 1))
+		if (charlist_resize(pcv, 1))
 			return 1;
 	*(pcv->data + pcv->length) = value;
 	pcv->length++;
 	return 0;
 }
 
-int charlist_pusharray(CharList *pcv, const char *src, size_t len) {
+int langM_charlist_pusharray(LangM_CharList *pcv, const char *src, size_t len) {
 	if (!pcv || !src)
 		return 1;
 	if (pcv->length + len > pcv->size)
-		if (_charlist_resize(pcv, len))
+		if (charlist_resize(pcv, len))
 			return 1;
 	memcpy(pcv->data + pcv->length, src, len);
 	pcv->length += len;
 	return 0;
 }
 
-int charlist_setarray(CharList *pcv, size_t index, const char *src, size_t len) {
+int langM_charlist_setarray(LangM_CharList *pcv, size_t index, const char *src, size_t len) {
 	if (!pcv || !src)
 		return 1;
 	if (index + len > pcv->size)
-		if (_charlist_resize(pcv, len))
+		if (charlist_resize(pcv, len))
 			return 1;
 	memcpy(pcv->data + index, src, len);
 	return 0;
--- ProgrammingLanguage/charlist.h
+++ ProgrammingLanguage/charlist.h
@@ -7,14 +7,14 @@ typedef struct {
 	char* data;
 	size_t length;
 	size_t size;
-} CharList;
+} LangM_CharList;
 
-int charlist_init(CharList *pcv, size_t size);
-int charlist_free(CharList *pcv);
-int charlist_get(CharList *pcv, size_t index, char *dst);
-int charlist_pop(CharList *pcv, char *dst);
-int charlist_push(CharList *pcv, char value);
-int charlist_pusharray(CharList *pcv, const char *src, size_t len);
-int charlist_setarray(CharList *pcv, size_t index, const char *src, size_t len);
+int langM_charlist_init(LangM_CharList *pcv, size_t size);
+int langM_charlist_free(LangM_CharList *pcv);
+int langM_charlist_get(LangM_CharList *pcv, size_t index, char *dst);
+int langM_charlist_pop(LangM_CharList *pcv, char *dst);
+int langM_charlist_push(LangM_CharList *pcv, char value);
+int langM_charlist_pusharray(LangM_CharList *pcv, const char *src, size_t len);
+int langM_charlist_setarray(LangM_CharList *pcv, size_t index, const char *src, size_t len);
 
 #endif // CHARLIST_H
