9ae68ffbe2856053469af9e5e82be424acbfab4f
xgr369 <example@google.com>
2026-08-17 10:19:12 -0400

    renamed list to langm_list, add clear function

--- ProgrammingLanguage/list.c
+++ ProgrammingLanguage/list.c
@@ -1,16 +1,16 @@
 #include "list.h"
 #include <string.h>
 
-int _list_resize(List *pv) {
-	pv->size = pv->size ? pv->size * 2 : 1;
-	void* ptr = realloc(pv->data, pv->size * pv->elemSize);
+static int list_resize(LangM_List *pv, size_t size) {
+	pv->size = size;
+	void *ptr = realloc(pv->data, pv->size * pv->elemSize);
 	if (!ptr)
 		return 1;
 	pv->data = ptr;
 	return 0;
 }
 
-int list_init(List *pv, size_t elemSize, size_t size) {
+int langM_list_init(LangM_List *pv, size_t elemSize, size_t size) {
 	if (!pv || elemSize == 0)
 		return 1;
 	pv->data = malloc(size * elemSize);
@@ -22,7 +22,16 @@ int list_init(List *pv, size_t elemSize, size_t size) {
 	return 0;
 }
 
-int list_free(List *pv) {
+int langM_list_clear(LangM_List *pv, size_t size) {
+	if (!pv)
+		return 1;
+	if (list_resize(pv, size))
+		return 1;
+	pv->length = 0;
+	return 0;
+}
+
+int langM_list_free(LangM_List *pv) {
 	if (!pv || !pv->data)
 		return 1;
 	free(pv->data);
@@ -30,20 +39,20 @@ int list_free(List *pv) {
 	return 0;
 }
 
-void *list_at(List *pv, size_t index) {
+void *langM_list_at(LangM_List *pv, size_t index) {
 	if (!pv || index < 0 || index >= pv->length)
 		return NULL;
 	return pv->data + pv->elemSize * index;
 }
 
-int list_get(List *pv, size_t index, void *dst) {
+int langM_list_get(LangM_List *pv, size_t index, void *dst) {
 	if (!pv || !dst || index < 0 || index >= pv->length)
 		return 1;
 	memcpy(dst, pv->data + index * pv->elemSize, pv->elemSize);
 	return 0;
 }
 
-int list_pop(List *pv, void *dst) {
+int langM_list_pop(LangM_List *pv, void *dst) {
 	if (!pv || pv->length == 0)
 		return 1;
 	if (dst)
@@ -52,25 +61,25 @@ int list_pop(List *pv, void *dst) {
 	return 0;
 }
 
-int list_popn(List *pv, size_t n) {
+int langM_list_popn(LangM_List *pv, size_t n) {
 	if (!pv || n < 0 || pv->length < n)
 		return 1;
 	pv->length -= n;
 	return 0;
 }
 
-int list_push(List *pv, const void *src) {
+int langM_list_push(LangM_List *pv, const void *src) {
 	if (!pv || !src)
 		return 1;
 	if (pv->length * pv->elemSize >= pv->size)
-		if (_list_resize(pv))
+		if (list_resize(pv, pv->size ? pv->size * 2 : 1))
 			return 1;
 	memcpy(pv->data + pv->length * pv->elemSize, src, pv->elemSize);
 	pv->length++;
 	return 0;
 }
 
-int list_removen(List *pv, size_t index, size_t n) {
+int langM_list_removen(LangM_List *pv, size_t index, size_t n) {
 	if (!pv || index > pv->length || n > pv->length - index)
 		return 1;
 	size_t tailSize = pv->length - index - n;
@@ -79,7 +88,7 @@ int list_removen(List *pv, size_t index, size_t n) {
 	return 0;
 }
 
-int list_set(List *pv, size_t index, void *src) {
+int langM_list_set(LangM_List *pv, size_t index, void *src) {
 	if (!pv || !src || index >= pv->length)
 		return 1;
 	memcpy(pv->data + index * pv->elemSize, src, pv->elemSize);
--- ProgrammingLanguage/list.h
+++ ProgrammingLanguage/list.h
@@ -8,16 +8,17 @@ typedef struct {
 	size_t elemSize;
 	size_t length;
 	size_t size;
-} List;
+} LangM_List;
 
-int list_init(List *pv, size_t elemSize, size_t size);
-int list_free(List *pv);
-void *list_at(List *pv, size_t index);
-int list_get(List *pv, size_t index, void *dst);
-int list_pop(List *pv, void *dst);
-int list_popn(List *pv, size_t n);
-int list_push(List *pv, const void *src);
-int list_removen(List *pv, size_t index, size_t n);
-int list_set(List *pv, size_t index, void *src);
+int langM_list_init(LangM_List *pv, size_t elemSize, size_t size);
+int langM_list_clear(LangM_List *pv, size_t size);
+int langM_list_free(LangM_List *pv);
+void *langM_list_at(LangM_List *pv, size_t index);
+int langM_list_get(LangM_List *pv, size_t index, void *dst);
+int langM_list_pop(LangM_List *pv, void *dst);
+int langM_list_popn(LangM_List *pv, size_t n);
+int langM_list_push(LangM_List *pv, const void *src);
+int langM_list_removen(LangM_List *pv, size_t index, size_t n);
+int langM_list_set(LangM_List *pv, size_t index, void *src);
 
 #endif // LIST_H
