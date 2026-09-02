f9042fc3f334cae569153ccf98689b5d30b5040b
xgr369 <example@google.com>
2026-09-01 21:56:45 -0400

    renamed "hash" to "table"

--- ProgrammingLanguage/table.h
+++ ProgrammingLanguage/table.h
@@ -1,8 +1,29 @@
-#ifndef TABLE_H
-#define TABLE_H
+#ifndef STRING_HASH_TABLE_H
+#define STRING_HASH_TABLE_H
 
-#include "lang.h"
+#include <stdlib.h>
 
-// WIP
+struct LangM_TableNode {
+	char *key;
+	struct LangM_TableNode *next;
+	char value[];
+};
+typedef struct LangM_TableNode LangM_TableNode;
 
-#endif // TABLE_H
+typedef struct {
+	LangM_TableNode **data;
+	size_t elemSize;
+	size_t size;
+	size_t count;
+} LangM_Table;
+
+int langM_table_init(LangM_Table *pt, size_t elemSize, size_t size);
+int langM_table_clear(LangM_Table *pt, size_t size);
+int langM_table_free(LangM_Table *pt);
+void *langM_table_at(LangM_Table *pt, const char *key);
+int langM_table_containskey(LangM_Table *pt, const char *key);
+int langM_table_get(LangM_Table *pt, const char *key, void *dst);
+int langM_table_put(LangM_Table *pt, const char *key, const void *src);
+int langM_table_remove(LangM_Table *pt, const char *key, void *dst);
+
+#endif // STRING_HASH_TABLE_H
